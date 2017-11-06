0acb54203c1a1db03e22fd0e1996fa6a949fb977
Xingxing liu <Ryanix@Xingxings-MacBook-Pro.local>
2017-11-06 16:37:33 -0500

    first commit

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 
 
